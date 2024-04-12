8fde853f7c67377dbc2536e017bd549c12a06408
etozhefifan <94104800+etozhefifan@users.noreply.github.com>
2024-04-12 17:35:36 +0300

    fix clipboard feature

---
+++ pastehook.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"github.com/atotto/clipboard"
+)
+
+const (
+	apiurl     = "https://pastebin.com/api/api_post.php"
+	api_option = "paste"
+)
+
+
+func inputArgs() (string, string) {
+	fileToOpen := os.Args[1]
+	LinesToSend := os.Args[2]
+	return fileToOpen, LinesToSend
+}
+
+func splitInput(linesToSend string) (int64, int64) {
+	strSlice := strings.Split(linesToSend, "-")
+	number1, err := strconv.ParseInt(strSlice[0], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	number2, err := strconv.ParseInt(strSlice[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if (number2 - number1) < 0{
+		panic("second value should be bigger than first")
+	}
+	return number1, number2
+
+}
+
+func parseFile(fileToOpen string, linesToSend string) string {
+	lineStart, lineEnd := splitInput(linesToSend)
+	file, err := os.Open(fileToOpen)
+	if err != nil {
+		panic(err)
+	}
+	red := io.Reader(file)
+	neccessaryLines := scanLines(red, lineStart, lineEnd)
+	file.Close()
+	return neccessaryLines
+}
+
+func scanLines(red io.Reader, lineStart int64, lineEnd int64)string{
+	scanner := bufio.NewScanner(red)
+	scanner.Split(bufio.ScanLines)
+	var buffer strings.Builder
+	var line int64
+	for scanner.Scan(){
+		line++
+		if line >= lineStart{
+			buffer.WriteString(scanner.Text())
+			buffer.WriteString("\n")
+		}
+		if line == lineEnd{
+			break
+		}
+			
+	}
+	return buffer.String()
+}
+
+
+func getDevKey() string {
+	le, found := os.LookupEnv("API_DEV_KEY")
+	if (found != true) || le == "" {
+		fmt.Println("API_DEV_KEY is not set and not found in envs")
+		panic(found)
+	}
+	api_dev_key := os.Getenv("API_DEV_KEY")
+	return api_dev_key
+}
+
+func formData(text string)url.Values{
+	v := url.Values{}
+	v.Set("api_dev_key", getDevKey())
+	v.Set("api_option", api_option)
+	v.Set("api_paste_code", text)
+	return v
+}
+
+func sendTextToPastehook(data url.Values)[]byte{
+	request, err := http.NewRequest(http.MethodPost, apiurl, bytes.NewBuffer([]byte(data.Encode())))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		panic(err)
+	}
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	return responseBody
+}
+
+func putLinkToClipboard(link string){
+	err := clipboard.WriteAll(link)
+	if err != nil {
+		fmt.Println("Error with copying link to buffer")
+		panic(err)
+	}
+}
+
+func main() {
+	fileToOpen, linesToSend := inputArgs()
+	text := parseFile(fileToOpen, linesToSend)
+	data := formData(text)
+	putLinkToClipboard(string(sendTextToPastehook(data)))
+}
